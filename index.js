a4aaf9706c0deeba3d967da7da9cdf2ad3cbaebd
gaboliveiradev <g38789138@gmail.com>
2023-04-20 10:59:47 -0300

    fazendo as exclusões de uma array  a partir de uma rota

--- index.js
+++ index.js
@@ -12,11 +12,25 @@ app.set('view engine', 'html');
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views')); // Setando a pasta onde ficará minhas views. (mesmo role para pegar o dir completo)
 
+var tarefas = ['Arrumar o Quarto', 'Comprar no Supermercado'];
+
 // Criando a rota "/" e definindo uma função de callback, onde ela recebe dois parametros:
 // req = requisição
 // res = resposta da rota
 app.get('/', (req, res)=>{
-    res.render('index', {nome:'Mariane'});
+    res.render('index', {arr_tarefas: tarefas});
+})
+
+// req.params.id: Explicando
+// req : objeto da nossa requisição
+// params : o que está vindo de parametro na nossa url
+// id : referente ao nosso parametro (:id)
+app.get('/delete/:id', (req, res)=>{
+    // Pego o array tarefas e faço um "clone" dela mas com uma verificação.
+    tarefas = tarefas.filter((val, index)=>{
+        if(index != req.params.id) return val;
+    })
+    res.render('index', {arr_tarefas: tarefas});
 })
 
 // Definimos que nosso servidor irá rodar na porta 5000.
